4b292d91dc07bba100184b5e5b22c3e084969edb
Sudipta Sharif <sudiptasharif@gmail.com>
2020-10-18 09:06:55 +0600

    Refactored code to create db tables and insert data in DatabaseHelper. Fixed login code in MainActivity. Removed addRecord from SystemUser.

--- app/src/main/java/com/example/arlingtonrentacar/users/SystemUser.java
+++ app/src/main/java/com/example/arlingtonrentacar/users/SystemUser.java
@@ -46,30 +46,4 @@ public class SystemUser {
         Cursor res =  db.rawQuery("select * from system_users where username = '" + userName + "' and password = '" + password + "';", null );
         return res;
     }
-    public String addRecord(DatabaseHelper databaseHelper,String firstname, String lastname, String username,String password,String UTAID,String role,
-                            String email,String phone,String street_address, String city, String state,String zipcode, String arlington_auto_member)
-    {
-        SQLiteDatabase db = databaseHelper.getWritableDatabase();
-        ContentValues cv = new ContentValues();
-        cv.put("firstname",firstname);
-        cv.put("lastname",lastname);
-        cv.put("username",username);
-        cv.put("password",password);
-        cv.put("UTAID",UTAID);
-        cv.put("role",role);
-        cv.put("email",email);
-        cv.put("phone",phone);
-        cv.put("street_address",street_address);
-        cv.put("city",city);
-        cv.put("state",state);
-        cv.put("zipcode",zipcode);
-        cv.put("arlington_auto_member",arlington_auto_member);
-
-        long res =  db.insert("system_users", null, cv );;
-        if(res== -1)
-            return "failed";
-        else
-            return "Account Created Successfully";
-
-    }
 }
