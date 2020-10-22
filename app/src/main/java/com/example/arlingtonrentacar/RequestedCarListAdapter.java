0bfa9e0a17dd21c4a613696f07f1d5cdb955a0a0
Sudipta Sharif <sudiptasharif@gmail.com>
2020-10-22 22:20:10 +0600

    Done with Requested Car Details for Iteration 2

--- app/src/main/java/com/example/arlingtonrentacar/RequestedCarListAdapter.java
+++ app/src/main/java/com/example/arlingtonrentacar/RequestedCarListAdapter.java
@@ -1,6 +1,7 @@
 package com.example.arlingtonrentacar;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,40 +13,50 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.LinkedList;
 
 public class RequestedCarListAdapter extends RecyclerView.Adapter<RequestedCarListAdapter.RequestedCarViewHolder> {
-    private final LinkedList<String> rCarList;
-    private LayoutInflater rInflater;
+    private final LinkedList<String> mCarList;
+    private LayoutInflater mInflater;
+    private Context context;
 
     public RequestedCarListAdapter(Context context, LinkedList<String> carList){
-        rInflater = LayoutInflater.from(context);
-        this.rCarList = carList;
+        mInflater = LayoutInflater.from(context);
+        this.mCarList = carList;
     }
 
     @NonNull
     @Override
     public RequestedCarViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View rItemView = rInflater.inflate(R.layout.requestedcarlist_rowitem, parent, false);
+        context = parent.getContext();
+        View rItemView = mInflater.inflate(R.layout.requestedcarlist_rowitem, parent, false);
         return new RequestedCarViewHolder(rItemView, this);
     }
 
     @Override
     public void onBindViewHolder(@NonNull RequestedCarViewHolder holder, int position) {
-        String rCurrent = rCarList.get(position);
+        String rCurrent = mCarList.get(position);
         holder.requestedCarItemView.setText(rCurrent);
     }
 
     @Override
     public int getItemCount() {
-        return rCarList.size();
+        return mCarList.size();
     }
 
-    class RequestedCarViewHolder extends RecyclerView.ViewHolder{
+    class RequestedCarViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
         public final TextView requestedCarItemView;
-        final RequestedCarListAdapter rAdapter;
+        final RequestedCarListAdapter mAdapter;
 
         public RequestedCarViewHolder(View itemView, RequestedCarListAdapter adapter){
             super(itemView);
             requestedCarItemView = itemView.findViewById(R.id.requestedcar_rowitem);
-            this.rAdapter = adapter;
+            this.mAdapter = adapter;
+            requestedCarItemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View view) {
+            // TODO: for Interation 3 will need to change this, for now just call the target activity
+            Intent intent = new Intent(context, RequestedCarDetailsActivity.class);
+            context.startActivity(intent);
         }
     }
 
